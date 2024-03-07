81167572a179b0318dd3f52182b0bab99cbdc186
shellyn - intern <shellyneuriska@gmail.com>
2024-03-08 06:16:33 +0700

    commit tp masih error

---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    nama: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    nik: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        len: [16],
+      },
+    },
+    noHP: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: /^\d{9,13}$/,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ktp: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    statusValidate: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  });
+
+  return User;
+};
+
+module.exports = UserModel;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.sequelize.sync().then(() => {
+  console.log("MySQL database connected");
+});
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
