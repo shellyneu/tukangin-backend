b3c5e9ba5a83af65796412f07c2de016bba6b650
shellyn - intern <shellyneuriska@gmail.com>
2024-03-21 14:06:48 +0700

    fix upload bukti

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { authentication } = require("../middleware/authMiddleware");
+const uploadPhoto = require("../middleware/imageMiddleware");
 const {
   createTransaction,
   getTransactions,
 } = require("../controllers/transactionController");
 
 // Routes
-router.post("/:tukangId/:jobId", authentication, createTransaction);
+router.post(
+  "/:tukangId/:jobId",
+  authentication,
+  uploadPhoto("bukti").single("bukti"),
+  createTransaction
+);
 router.get("/", authentication, getTransactions);
 
 module.exports = router;
