a105fa13705561dbc13053ed0005744d06d84541
shellyn - intern <shellyneuriska@gmail.com>
2024-03-23 11:04:22 +0700

    move server.js

--- src/server.js
+++ src/server.js
@@ -8,16 +8,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./models");
+const db = require("../models");
 db.sequelize.sync().then(() => {
   console.log("MySQL database connected");
 });
 
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/tukang", require("./routes/tukangRoutes"));
-app.use("/api/postJobs", require("./routes/postJobRoutes"));
-app.use("/api/reviews", require("./routes/reviewRoutes"));
-app.use("/api/transactions", require("./routes/transactionRoutes"));
+app.use("/api/users", require("../routes/userRoutes"));
+app.use("/api/tukang", require("../routes/tukangRoutes"));
+app.use("/api/postJobs", require("../routes/postJobRoutes"));
+app.use("/api/reviews", require("../routes/reviewRoutes"));
+app.use("/api/transactions", require("../routes/transactionRoutes"));
 
 app.use(express.static("storage"));
 
